3f5b2c2526c2d20d8bbd416863d0c9cf1e912837
Zulhilmi Zainudin <zulh@civo.com>
2021-03-01 11:33:50 +0800

    Added 'bizaar version' command and enable nfpms releaser

---
+++ cmd/version.go
@@ -0,0 +1,76 @@
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	quiet   bool
+	verbose bool
+
+	// VersionCli is set from outside using ldflags
+	VersionCli = "0.0.0"
+
+	// CommitCli is set from outside using ldflags
+	CommitCli = "none"
+
+	// DateCli is set from outside using ldflags
+	DateCli = "unknown"
+
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Version will output the current build information",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			// TODO - uncomment this after we go live
+			// githubTag := &latest.GithubTag{
+			// 	Owner:             "civo",
+			// 	Repository:        "bizaar-cli",
+			// 	FixVersionStrFunc: latest.DeleteFrontV(),
+			// }
+			switch {
+			case verbose:
+				fmt.Printf("Client version: v%s\n", VersionCli)
+				fmt.Printf("Go version (client): %s\n", runtime.Version())
+				fmt.Printf("Build date (client): %s\n", DateCli)
+				fmt.Printf("Git commit (client): %s\n", CommitCli)
+				fmt.Printf("OS/Arch (client): %s/%s\n", runtime.GOOS, runtime.GOARCH)
+
+				// TODO - uncomment this after we go live
+				// res, err := latest.Check(githubTag, strings.Replace(VersionCli, "v", "", 1))
+				// if err != nil {
+				// 	utility.Error("Checking for a newer version failed with %s", err)
+				// 	os.Exit(1)
+				// }
+
+				// if res.Outdated {
+				// 	utility.RedConfirm("A newer version (v%s) is available, please upgrade\n", res.Current)
+				// }
+			case quiet:
+				fmt.Printf("v%s\n", VersionCli)
+			default:
+				fmt.Printf("v%s\n", VersionCli)
+
+				// TODO - uncomment this after we go live
+				// res, err := latest.Check(githubTag, strings.Replace(VersionCli, "v", "", 1))
+				// if err != nil {
+				// 	utility.Error("Checking for a newer version failed with %s", err)
+				// 	os.Exit(1)
+				// }
+
+				// if res.Outdated {
+				// 	utility.RedConfirm("A newer version (v%s) is available, please upgrade\n", res.Current)
+				// }
+			}
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+	versionCmd.Flags().BoolVarP(&quiet, "quiet", "q", false, "Use quiet output for simple output")
+	versionCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Use verbose output to see full information")
+}
