c48ebb63cd960d2c29945ef862ed3bbda0d571e3
Zulhilmi Zainudin <zulh@civo.com>
2021-11-22 14:54:10 +0800

    Remove unused code

--- cmd/client-helper.go
+++ cmd/client-helper.go
@@ -11,11 +11,6 @@ import (
 // Namespace where we save App custom resource
 var targetNamespace = "kubemart-system"
 
-// KmClientset is used as receiver object in few functions below
-// type KmClientset struct {
-// 	*kubemartclient.Clientset
-// }
-
 func checkIfCrdExists() error {
 	crdExist, err := utils.IsCRDExist("apps.kubemart.civo.com")
 
@@ -48,71 +43,3 @@ func NewClientFromLocalKubeConfig() (*kubemartclient.Clientset, error) {
 
 	return clientset, nil
 }
-
-// func NewClientFromLocalKubeConfig() (*KmClientset, error) {
-// 	err := checkIfCrdExists()
-// 	if err != nil {
-// 		return &KmClientset{}, err
-// 	}
-
-// 	restconfig, err := utils.GetRESTConfig()
-// 	if err != nil {
-// 		return &KmClientset{}, err
-// 	}
-
-// 	clientset, err := kubemartclient.NewForConfig(restconfig)
-// 	if err != nil {
-// 		return &KmClientset{}, fmt.Errorf("unable to create k8s clientset - %v", err)
-// 	}
-
-// 	return &KmClientset{clientset}, nil
-// }
-
-// // CreateApp will create an App in user's cluster
-// func (cs *KmClientset) CreateApp(appName string, plan string) (*v1alpha1.App, error) {
-// 	return cs.Clientset.KubemartV1alpha1().Apps(targetNamespace).Create(
-// 		context.Background(),
-// 		&v1alpha1.App{
-// 			ObjectMeta: metav1.ObjectMeta{
-// 				Name: appName,
-// 			},
-// 			Spec: v1alpha1.AppSpec{
-// 				Name:   appName,
-// 				Action: "install",
-// 				Plan:   plan,
-// 			},
-// 		},
-// 		v1.CreateOptions{},
-// 	)
-// }
-
-// // GetApp will get an App from user's cluster
-// func (cs *KmClientset) GetApp(appName string) (*v1alpha1.App, error) {
-// 	return cs.Clientset.KubemartV1alpha1().Apps(targetNamespace).Get(context.Background(), appName, v1.GetOptions{})
-// }
-
-// // ListApps will get all Apps from user's cluster
-// func (cs *KmClientset) ListApps() (*v1alpha1.AppList, error) {
-// 	return cs.Clientset.KubemartV1alpha1().Apps(targetNamespace).List(context.Background(), v1.ListOptions{})
-// }
-
-// // UpdateApp will update an App in user's cluster
-// func (cs *KmClientset) UpdateApp(appName string) (*v1alpha1.App, error) {
-// 	return cs.Clientset.KubemartV1alpha1().Apps(targetNamespace).Update(
-// 		context.Background(),
-// 		&v1alpha1.App{
-// 			ObjectMeta: metav1.ObjectMeta{
-// 				Name: appName,
-// 			},
-// 			Spec: v1alpha1.AppSpec{
-// 				Action: "update",
-// 			},
-// 		},
-// 		v1.UpdateOptions{},
-// 	)
-// }
-
-// // DeleteApp will delete an App from user's cluster
-// func (cs *KmClientset) DeleteApp(appName string) error {
-// 	return cs.Clientset.KubemartV1alpha1().Apps(targetNamespace).Delete(context.Background(), appName, v1.DeleteOptions{})
-// }
